d479a1f410817b0330cc11dc8e3333019c6b1876
Ethan Chiou <ethan@span.io>
2022-06-07 20:14:38 -0700

    Set state in useEffect

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -3,9 +3,9 @@ import { authEndpoint, clientId, redirectUri, scopes } from "./config";
 import GraphScreen from "./GraphScreen";
 
 const App = () => {
-  const [token, setToken] = useState(null);
+  let token = "";
 
-  useEffect(() => {
+  if (typeof window !== "undefined") {
     var mToken = window.location.hash
       .substring(1)
       .split("&")
@@ -17,29 +17,26 @@ const App = () => {
         return initial;
       }, {})['access_token'];
     if (mToken) {
-      setToken(mToken);
+      token = mToken;
     }
+  }
 
-  })
   return (
-    <div>
-
-      <body className="App-body">
-        {!token && (
-          <a
-            className="btn btn--loginApp-link"
-            href={`${authEndpoint}?client_id=${clientId}&redirect_uri=${redirectUri}&scope=${scopes.join(
-              "%20"
-            )}&response_type=token&show_dialog=true`}
-          >
-            Login to Spotify
-          </a>
-        )}
-        {token && (
-          <GraphScreen token={token} />
-        )}
-      </body>
-    </div>
+    <body className="App-body">
+      {!token && (
+        <a
+          className="btn btn--loginApp-link"
+          href={`${authEndpoint}?client_id=${clientId}&redirect_uri=${redirectUri}&scope=${scopes.join(
+            "%20"
+          )}&response_type=token&show_dialog=true`}
+        >
+          Login to Spotify
+        </a>
+      )}
+      {token && (
+        <GraphScreen token={token} />
+      )}
+    </body>
   );
 
 }
