c2f2a99204690647be45b5828ab3ecd0bbc6e905
Ethan Chiou <ethan@span.io>
2022-06-07 23:01:23 -0700

    Remove unnecessary from GraphScreen

--- src/components/GraphScreen.js
+++ src/components/GraphScreen.js
@@ -1,28 +1,8 @@
 import React, { useState, useEffect } from "react";
 export default function GraphScreen({ token }) {
-    // const [artistToGenres, setArtistToGenres] = useState([]);
-    // const [dataObj, setDataObj] = useState({});
     const axios = require("axios");
-    // const [piData, setPiData] = useState({})
     const [isMounted, setIsMounted] = useState(false);
 
-    const indexOfMax = (arr) => {
-        if (arr.length === 0) {
-            return -1;
-        }
-
-        var max = arr[0];
-        var maxIndex = 0;
-
-        for (var i = 1; i < arr.length; i++) {
-            if (arr[i] > max) {
-                maxIndex = i;
-                max = arr[i];
-            }
-        }
-
-        return maxIndex;
-    }
     useEffect(() => {
         let topArtists = [];
         let topTracks = [];
